c46faeba08783673a3bece3bd5e30ca27ec5921a
araikes <adam.raikes@gmail.com>
2023-03-28 19:41:46 -0700

    Update utils.py
    testing

--- brkraw/lib/utils.py
+++ brkraw/lib/utils.py
@@ -407,8 +407,8 @@ def build_bids_json(dset, row, fname, json_path, slope=False, offset=False):
                 condition = ['dwi', None]
             elif re.search('func', row.modality, re.IGNORECASE):
                 condition = ['func', None]
-            elif re.search('anat', row.DataType, re.IGNORECASE):
-                condition = ['anat', None]
+            #elif re.search('anat', row.DataType, re.IGNORECASE):
+            #    condition = ['anat', None]
             else:
                 condition = None
             if re.search('magnitude', row.modality, re.IGNORECASE):
